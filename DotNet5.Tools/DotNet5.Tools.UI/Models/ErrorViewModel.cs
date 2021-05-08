27d6ab5776197c128e5dc53be5eff0984f94d837
ibrahimschi6 <76202393+ibrahimschi6@users.noreply.github.com>
2021-05-08 04:37:30 +0200

    add mvc template UI project

---
+++ DotNet5.Tools/DotNet5.Tools.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DotNet5.Tools.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
